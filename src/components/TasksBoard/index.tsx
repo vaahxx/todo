4522f85c1ca3a237da4b2616af215a84f25d87fa
Valentina Garavaglia <vaahxx@gmail.com>
2022-12-04 19:52:00 -0500

    adjust styles

--- src/components/TasksBoard/index.tsx
+++ src/components/TasksBoard/index.tsx
@@ -1,7 +1,23 @@
+import { useState } from "react";
 import plusIcon from "../../assets/plus.svg";
+import clipboard from "../../assets/clipboard.svg";
 import styles from "./TasksBoard.module.css";
 
 export function TasksBoard() {
+  const [tasks, setTasks] = useState<
+    { id: string; name: string; isComplete: boolean }[]
+  >([]);
+  const [createdTasksCounter, setCreatedTasksCounter] = useState(0);
+  const [concludedTasksCounter, setConcludedTasksCounter] = useState(0);
+
+  const EmptyBoard = () => (
+    <div className={styles.emptyBoard}>
+      <img src={clipboard} alt="clipboard-icon"></img>
+      <strong>You don't have any task yet</strong>
+      <p>Create tasks and organize your to-dos</p>
+    </div>
+  );
+
   return (
     <div className={styles.wrapper}>
       <form className={styles.form}>
@@ -11,6 +27,21 @@ export function TasksBoard() {
           <img src={plusIcon} alt="plus-icon"></img>
         </button>
       </form>
+      <div className={styles.progress}>
+        <div>
+          <span>Created Tasks</span>
+          <strong>{createdTasksCounter}</strong>
+        </div>
+        <div>
+          <span>Concluded</span>
+          <strong>{concludedTasksCounter}</strong>
+        </div>
+      </div>
+      {!tasks.length ? (
+        <EmptyBoard />
+      ) : (
+        <div className={styles.tasksBoard}></div>
+      )}
     </div>
   );
 }
