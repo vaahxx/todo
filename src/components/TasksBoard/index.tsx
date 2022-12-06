6c7f816ad8ba42d3d890c32570fb958a17aa1a85
Valentina Garavaglia <vaahxx@gmail.com>
2022-12-06 16:43:31 -0500

    fix toggle checkbox

--- src/components/TasksBoard/index.tsx
+++ src/components/TasksBoard/index.tsx
@@ -1,14 +1,37 @@
 import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 import plusIcon from "../../assets/plus.svg";
 import clipboard from "../../assets/clipboard.svg";
 import styles from "./TasksBoard.module.css";
 
 export function TasksBoard() {
   const [tasks, setTasks] = useState<
-    { id: string; name: string; isComplete: boolean }[]
+    { id: string; content: string; isComplete: boolean }[]
   >([]);
   const [createdTasksCounter, setCreatedTasksCounter] = useState(0);
   const [concludedTasksCounter, setConcludedTasksCounter] = useState(0);
+
+  function handleCreateTask(ev: React.SyntheticEvent<HTMLFormElement>) {
+    ev.preventDefault();
+    const value = ev.currentTarget.content.value;
+    console.log(ev.currentTarget);
+    setTasks([
+      ...tasks,
+      {
+        id: uuidv4(),
+        content: value,
+        isComplete: false,
+      },
+    ]);
+    setCreatedTasksCounter((previous) => previous + 1);
+  }
+
+  function handleCheckboxChange(taskId: string) {
+    const tasksCopy = [...tasks];
+    const index = tasks.findIndex((task) => task.id === taskId);
+    tasksCopy[index].isComplete = !tasks[index].isComplete;
+    setTasks([...tasksCopy]);
+  }
 
   const EmptyBoard = () => (
     <div className={styles.emptyBoard}>
@@ -20,8 +43,8 @@ export function TasksBoard() {
 
   return (
     <div className={styles.wrapper}>
-      <form className={styles.form}>
-        <textarea placeholder="Add new task" />
+      <form className={styles.form} onSubmit={handleCreateTask}>
+        <textarea placeholder="Add new task" name="content" />
         <button type="submit">
           Create
           <img src={plusIcon} alt="plus-icon"></img>
@@ -40,7 +63,21 @@ export function TasksBoard() {
       {!tasks.length ? (
         <EmptyBoard />
       ) : (
-        <div className={styles.tasksBoard}></div>
+        <div className={styles.tasksBoard}>
+          {tasks.map((task) => (
+            <div key={task.id}>
+              <input
+                type="checkbox"
+                id={task.id}
+                name={task.content}
+                value={task.content}
+                checked={task.isComplete}
+                onChange={() => handleCheckboxChange(task.id)}
+              />
+              <p>{task.content}</p>
+            </div>
+          ))}
+        </div>
       )}
     </div>
   );
